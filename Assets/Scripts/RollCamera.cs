b57ea8258cecb6f6c66bc1db829680dc8add2fbb
AtapinAV <atap03081991@mail.ru>
2023-11-29 22:49:49 +0400

    AddCamera3d

---
+++ Assets/Scripts/RollCamera.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject _camera;
+    [SerializeField] private GameObject _camera3D;
+
+    private bool _cameraROLL = true;
+
+    private void Update()
+    {
+        CameraRoll();
+    }
+
+    private void CameraRoll()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_cameraROLL)
+            {
+                _camera.SetActive(false);
+                _camera3D.SetActive(true);
+                _cameraROLL = false;
+            }
+            else
+            {
+                _camera.SetActive(true);
+                _camera3D.SetActive(false);
+                _cameraROLL = true;
+            }
+        }
+    }
+}
